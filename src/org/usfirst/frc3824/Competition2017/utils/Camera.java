956e3a17eb813892a4e41974d639eb33915ab0e3
Jackson Smith <rjsmith1999@gmail.com>
2017-02-01 20:18:17 -0500

    Some cleanup on Camera processing

--- src/org/usfirst/frc3824/Competition2017/utils/Camera.java
+++ src/org/usfirst/frc3824/Competition2017/utils/Camera.java
@@ -45,10 +45,6 @@ public class Camera
 		Mat contourImg = new Mat();
 		Mat hierarchy = new Mat();
 
-		// Vectors holding contour and contour attributes
-		// Vector<vector<Point> > inputContours;
-		// Vector<vector<Point> > outputContours;
-
 		List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
 
 		while (!Thread.interrupted())
@@ -62,9 +58,6 @@ public class Camera
 			double V_min = SmartDashboard.getNumber("V Min", 0);
 			double V_max = SmartDashboard.getNumber("V Max", 100);
 
-			// Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
-			// outputStream.putFrame(output);
-
 			// Blurs image from camera to make colors run together
 			Imgproc.blur(source, cameraFrameImage, new Size(10, 10));
 
@@ -73,61 +66,34 @@ public class Camera
 
 			// Makes the output image only show elements that are in between
 			// the specified HSV min and max constants
-			Core.inRange(cameraFrameImage, new Scalar(H_min, S_min, V_min), new Scalar(H_max, S_max, V_max),
-					output);
+//			Core.inRange(cameraFrameImage, new Scalar(H_min, S_min, V_min), new Scalar(H_max, S_max, V_max),
+//					output);
 
 			// Makes the contour image only show elements that are in
 			// between the specified HSV min and max constants
 			Core.inRange(cameraFrameImage, new Scalar(H_min, S_min, V_min), new Scalar(H_max, S_max, V_max),
 					contourImg);
 
-			// outputStream.putFrame(contourImg);
-
 			// Finds the contours on the contour image
 			Imgproc.findContours(contourImg, contours, hierarchy, Imgproc.RETR_EXTERNAL,
 					Imgproc.CHAIN_APPROX_SIMPLE);
 
-			// Find max contour area
-			Iterator<MatOfPoint> each = contours.iterator();
-			while (each.hasNext())
+			for (MatOfPoint contour : contours)
 			{
-				MatOfPoint wrapper = each.next();
-				double area = Imgproc.contourArea(wrapper);
-
-				System.out.println("Area: " + area);
-			}
-			
-//			contours.stream().map(Imgproc.contourArea);
-
-			for (int i = 0; i < contours.size(); i++)
-			{
-				System.out.println(Imgproc.contourArea(contours.get(i)));
-
-				// if (Imgproc.contourArea(contours.get(i)) > 50 )
-				// {
-				Rect rect = Imgproc.boundingRect(contours.get(i));
-
-				System.out.println(rect.height);
+				Rect rect = Imgproc.boundingRect(contour);
 
 				if (rect.height > 28)
 				{
 					System.out.println(rect.x + "," + rect.y + "," + rect.height + "," + rect.width);
+					
+					// draw rectangle around contour areas with large height
 					Imgproc.rectangle(contourImg, new Point(rect.x, rect.height), new Point(rect.y, rect.width),
 							new Scalar(0, 0, 255));
 				}
-				// }
 			}
-
-			// for (int outputContourIndex = 0; outputContourIndex <
-			// outputContours.size(); outputContourIndex++)
-			// {
-			// drawContours(contourImg, outputContours, outputContourIndex,
-			// CV_RGB(255, 255, 255), 2, 8, hierarchy, 0, Point());
-			// }
 
 			// Show the contours
 			outputStream.putFrame(contourImg);
-
 		}
 
 	});
