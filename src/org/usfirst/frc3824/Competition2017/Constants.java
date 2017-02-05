e4ef7cde77834b6c52c81b572312463b2b0f6890
JohnYoung <john.young3210@gmail.com>
2017-02-05 17:05:16 -0500

    Added support for the ball pickup, secondshooter and added comments to the rest of the source code.

--- src/org/usfirst/frc3824/Competition2017/Constants.java
+++ src/org/usfirst/frc3824/Competition2017/Constants.java
@@ -1,27 +1,62 @@
 package org.usfirst.frc3824.Competition2017;
 
-public class Constants {
+public class Constants
+{
 	// ***************************************************************************************
 	// Drive straight constants
-	public static double DRIVETRAIN_DRIVE_STRAIGHT_P          =  0.1;
-	public static double DRIVETRAIN_DRIVE_STRAIGHT_I          =  0.0;
-	public static double DRIVETRAIN_DRIVE_STRAIGHT_D          =  0.0;
-	
-	public static double DRIVETRAIN_DRIVE_MINIMUM_OUTPUT      = -1.0;
-	public static double DRIVETRAIN_DRIVE_MAXIMUM_OUTPUT      =  1.0;	
-	
-	public static double DRIVETRAIN_DRIVE_TOLERANCE           =  0.0;		
-	
+	public static double DRIVETRAIN_DRIVE_STRAIGHT_P		= 0.1;
+	public static double DRIVETRAIN_DRIVE_STRAIGHT_I		= 0.0;
+	public static double DRIVETRAIN_DRIVE_STRAIGHT_D		= 0.0;
+
+	public static double DRIVETRAIN_DRIVE_MINIMUM_OUTPUT	= -1.0;
+	public static double DRIVETRAIN_DRIVE_MAXIMUM_OUTPUT	= 1.0;
+
+	public static double DRIVETRAIN_DRIVE_TOLERANCE			= 0.0;
+
 	// ***************************************************************************************
 	// Chassis turn constants
-	
-	public static double TURN_ANGLE_P                         = 0.05;
-	public static double TURN_ANGLE_I                         = 0.0;
-	public static double TURN_ANGLE_D                         = 0.0;
-	
-	public static double TURN_ANGLE_MINIMUM_OUTPUT            = -0.9;
-	public static double TURN_ANGLE_MAXIMUM_OUTPUT            =  0.9;	
 
-	public static double TURN_THRESHOLD                       = 5.0;
+	public static double TURN_ANGLE_P						= 0.05;
+	public static double TURN_ANGLE_I						= 0.0;
+	public static double TURN_ANGLE_D						= 0.0;
 
+	public static double TURN_ANGLE_MINIMUM_OUTPUT			= -0.9;
+	public static double TURN_ANGLE_MAXIMUM_OUTPUT			= 0.9;
+
+	public static double TURN_THRESHOLD						= 5.0;
+
+	// ***************************************************************************************
+	// Shooter constants
+	
+	public static double SHOOTER_P							= 0.0050;
+	public static double SHOOTER_I							= 0.0004;
+	public static double SHOOTER_D							= 0.0000;
+	public static double SHOOTER_F							= 0.0000;
+	
+	// ***************************************************************************************
+	// Feeder constants
+	
+	public static double FEEDER_P							= 0.0050;
+	public static double FEEDER_I							= 0.0004;
+	public static double FEEDER_D							= 0.0000;
+	public static double FEEDER_F							= 0.0000;
+	
+	// ***************************************************************************************
+	// Ball Pickup constants
+	
+	public static double SWEEPER_VOLTAGE					= 5.0;
+	public static double TRANSPORT_VOLTAGE					= 5.0;
+
+	// ***************************************************************************************
+	// Ultrasonic constants
+
+	public static double ULTRASONIC_X1						= 0.086669;
+	public static double ULTRASONIC_Y1						= 10.0;
+
+	public static double ULTRASONIC_X2						= 0.467592;
+	public static double ULTRASONIC_Y2						= 50.0;
+
+	// ***************************************************************************************
+	// Target constants
+	public static double DEVIATION_FROM_TARGET				= 2; 
 }
--- src/org/usfirst/frc3824/Competition2017/RPiDataSource.java
+++ src/org/usfirst/frc3824/Competition2017/RPiDataSource.java
@@ -1,4 +1,5 @@
 package org.usfirst.frc3824.Competition2017;
+
 import java.lang.Thread;
 import java.net.*;
 
@@ -9,125 +10,164 @@ import java.io.*;
 /*
  * RPiDataSource is an object representing the RaspberryPi as a source of
  * Data for the RoboRio.  The connection to the RPi is via a network connection.
- * This object will handle all of the netowrking and cache values locally
+ * This object will handle all of the networking and cache values locally
  */
-public class RPiDataSource {
-	private	DatagramSocket socket = null;
-	private int bufLength = 256;
-	private boolean running = false;
-	private Target lastTarget;
-	private boolean mActive = false;
-	
-	/*
+public class RPiDataSource
+{
+	private DatagramSocket	socket		= null;
+	private int				bufLength	= 256;
+	private boolean			running		= false;
+	private boolean			mActive		= false;
+	private Target			lastTarget;
+
+	private static RPiDataSource instance = new RPiDataSource();
+
+	/**
 	 * Private Constructor to support a singleton object
 	 */
-	private RPiDataSource() {
-		
+	private RPiDataSource()
+	{
+
 	}
-	
-	private static RPiDataSource instance = new RPiDataSource();
-	public static RPiDataSource getInstance() {return instance;}
-	
-	
-	public boolean start() {
-		new Thread(null, ()->{
+
+	/**
+	 * Method to return the RPiDataSource class 
+	 */
+	public static RPiDataSource getInstance()
+	{
+		return instance;
+	}
+
+	/**
+	 * Start the RaspberryPi connection as a thread
+	 */
+	public boolean start()
+	{
+		// Start a thread for the RaspberryPi connection
+		new Thread(null, () ->
+		{
 			server();
 		}, "RpiDataSource").start();
-		
+
 		return true;
 	}
-	
+
 	/*
 	 * This is the server connection to the RaspberryPi
 	 */
-	private void server(){
-		
-		try{
+	private void server()
+	{
+		try
+		{
 			socket = new DatagramSocket(5800);
-			socket.setSoTimeout(500);    // receive packet will timeout if no packet is received for 500ms
+			socket.setSoTimeout(500);  // receive packet will timeout if no packet is received for 500ms
 			System.out.println("RPiDataSource listening on port: " + socket.getLocalPort());
-		} catch (java.io.IOException e) {
+			
+		} catch (java.io.IOException e)
+		{
 			System.err.println("Could not create datagram socket.");
 			return;
 		}
 
+		// Indicate that the connection is running
 		running = true;
-		
-		while(running) {
-			try {
-				byte[] buf = new byte[bufLength];
+
+		while (running)
+		{
+			try
+			{
+				byte[]         buffer = new byte[bufLength];
+				int            port;
 				DatagramPacket packet;
-				InetAddress address;
-				int port;
+				InetAddress    address;
 				
-				// receive a packet of data
-				packet = new DatagramPacket(buf, bufLength);
+				// Receive a packet of data
+				packet = new DatagramPacket(buffer, bufLength);
+				
 				try
 				{
 					socket.receive(packet);
 					address = packet.getAddress();
-					port = packet.getPort();
-					
+					port    = packet.getPort();
+
 					System.out.println("Received data from IP address: " + address + " port: " + port);
-					
+
 					// Store the data locally
-					lastTarget = new Target(buf);
-					
+					lastTarget = new Target(buffer);
+
 					mActive = true;
 					// send a response - not wanting to send a response at the moment
 					// packet = new DatagramPacket(buf, buf.length, address, port);
 					// socket.send(packet);
-				}
-				catch (SocketTimeoutException e)
+				} catch (SocketTimeoutException e)
 				{
 					mActive = false;
 				}
-			} catch (IOException e) {
-				System.err.println("IOException:  " + e);
+			} catch (IOException e)
+			{
+				System.err.println("IOException: " + e);
 				running = false;
 			}
 		}
-		
+
+		// Close the RaspberryPi connection
 		socket.close();
 	}
-	
+
+	/**
+	 * Method to return the last target data packet
+	 */
 	public Target getTarget()
 	{
 		return lastTarget;
 	}
-	
+
+	/**
+	 * Method to indicate if the connection to the RaspberryPi is active
+	 */
 	public boolean isActive()
 	{
 		return mActive;
 	}
-	
+
+	/**
+	 * Method to update the SmartDashboard with the target information
+	 */
 	public void updateSmartDashboardData()
 	{
-        try {
-        	if(getTarget().isValid()){
-		        SmartDashboard.putString("Target", new String("Idx: " + getTarget().getFrameIndex() 
-						+ "  Type: " + getTarget().getTargetType()
-						+ "  Center: "  + getTarget().getXCenter() + ", "  + getTarget().getYCenter()
-						+ "  Height: "  + getTarget().getHeight()) );
-        	}
-        	else {
-        		SmartDashboard.putString("NO VALID TARGET", null);
-        	}
-        }
-        catch (Exception e) {
-        	
-        }		
+		try
+		{
+			// Determine if the target information is valid
+			if (getTarget().isValid())
+			{
+				// Send the target information to the SmartDashboard
+				SmartDashboard.putString("Target",
+						new String("Idx: " + getTarget().getFrameIndex() + 
+								"  Type: " + getTarget().getTargetType() +
+								"  Center: " + getTarget().getXCenter() + ", " + 
+								getTarget().getYCenter() +
+								"  Height: " + getTarget().getHeight()));
+			} else
+			{
+				SmartDashboard.putString("NO VALID TARGET", null);
+			}
+		} catch (Exception e)
+		{
+
+		}
 	}
-	
+
+	/**
+	 * Method to indicate if the RaspberryPi connection is active
+	 */
 	public void updateSmartDashboardActive()
 	{
-		try {
+		try
+		{
 			SmartDashboard.putBoolean("RPiActive", isActive());
-		}
-		catch (Exception e) {
-			
+		} catch (Exception e)
+		{
+
 		}
 	}
 }
-
-
