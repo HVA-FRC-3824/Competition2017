e07434261c5cc4d31cc081f8fab2601282491d90
Bryson Gullett <brysongullett@gmail.com>
2017-06-12 15:37:00 -0400

    Removed content relating to the shooter, feeder, and ball pickup; changed instances of RobotDrive to use CAN Talons instead of PWM Speed Controllers

--- src/org/usfirst/frc3824/Competition2017/Constants.java
+++ src/org/usfirst/frc3824/Competition2017/Constants.java
@@ -6,8 +6,6 @@ public class Constants
 	// Drivetrain constants
 	public static double TWIST_MULTIPLIER     				=  0.90;
 	public static int    DRIVER_REVERSE_BUTTON				= 11;
-
-	public static double BACK_TO_SHOOT_DISTANCE 			= 18.0;
 	
 	// ***************************************************************************************
 	// Drive straight constants
@@ -68,55 +66,6 @@ public class Constants
 	public static double GEAR_PLACE_TIME                    =  1.0;
 	
 	// ***************************************************************************************
-	// Shooter constants
-	
-	public static double SHOOTER_P							= 0.0600;
-	public static double SHOOTER_I							= 0.0004;
-	public static double SHOOTER_D							= 4.0000;
-	public static double SHOOTER_F							= 0.0050;
-	
-
-	public static double SHOOTER_SPEED_HIGH                 = 4500.0; // Max speed
-	public static double SHOOTER_SPEED_MEDHIGH              = 3750.0;
-	public static double SHOOTER_SPEED_MEDLOW               = 3500.0;
-	public static double SHOOTER_SPEED_LOW                  = 3200.0;
-	
-	public static double DEFAULT_SHOOTER_SPEED              = SHOOTER_SPEED_MEDHIGH;
-	
-	public static double SHOOTER_B_OFFSET                   = -50.0;
-	
-	public static double SHOOTER_JOG_SPEED                  = 50.0;
-	
-	public static double FEEDER_CLEAR_BALLS_TIME            = 0.5;
-	public static double SHOOTER_SPINUP_TIME                = 1.0;
-	
-	public static double SHOOTER_UNJAM_SPEED                = -600.0;
-	
-	// ***************************************************************************************
-	// Feeder constants
-	
-	// PIDF constants are multiplied by 1000 when displayed on the SmartDashboard
-	public static double FEEDER_P							=  0.0400;
-	public static double FEEDER_I							=  0.0004;
-	public static double FEEDER_D							=  1.6000;
-	public static double FEEDER_F							=  0.0400;
-	public static double DEFAULT_FEEDER_SPEED               =  800.0;
-	public static double FEEDER_REVERSE_SPEED               = -200.0;
-	
-	public static double FEEDER_UNJAM_SPEED                 = -400.0;
-	
-	// ***************************************************************************************
-	// Ball Pickup constants
-	
-	public static double SWEEPER_VOLTAGE_PICKUP				= 1.0;
-	public static double TRANSPORT_VOLTAGE_PICKUP			= 1.5;
-	
-	public static double SWEEPER_VOLTAGE_REVERSE			= 1.0;
-	public static double TRANSPORT_VOLTAGE_REVERSE			= 1.5;
-	
-	public static double TRANSPORT_SLOW_VOLTAGE             = 0.5;
-	
-	// ***************************************************************************************
 	// Ultrasonic constants
 
 	public static double ULTRASONIC_X1						= 0.086669;
@@ -126,7 +75,7 @@ public class Constants
 	public static double ULTRASONIC_Y2						= 50.0;
 
 	// ***************************************************************************************
-	// Automated aim and shoot constants
+	// Automated aim constants
 	
 	public static double IMAGE_ANGLE_ENCODER_P              =  0.01;
 	public static double IMAGE_ANGLE_ENCODER_I              =  0.000;
